1de8c203460cea9e8b902b8456070da195b19d1d
Chris Cheng <kercheng@uiowa.edu>
2022-05-03 13:13:12 +0800

    Finished Languages

--- prac06/programming_language.py
+++ prac06/programming_language.py
@@ -8,3 +8,5 @@ class ProgrammingLanguage:
     def is_dynamic(self):
         return self.typing == 'Dynamic'
 
+    def __str__(self):
+        return '{}, {} Typing, Reflection={}, First appeared in {}'.format(self.name, self.typing, self.reflection, self.year)
